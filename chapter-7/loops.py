761a8523625f9f1fbf3f64a02fb4eab7900ab9c5
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-01-01 14:28:39 +0530

    chapter - 6,7 completed

---
+++ chapter-7/loops.py
@@ -0,0 +1,17 @@
+# for loop 
+for i in range(0,6):
+    print(i)
+
+# while loop
+i=0
+while (i<6):
+    print(i)
+    i+=1
+    
+l = [1,2,3,4,5]
+
+for item in l:
+    # print(item)
+    pass
+else:
+    print("Done")
